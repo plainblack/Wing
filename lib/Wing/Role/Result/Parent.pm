76319efcc179553ce89e8765ca3a0e17b5941713
JT Smith <jt@plainblack.com>
2012-02-26 18:28:37 -0600

    have to have fields defined before relationships

--- lib/Wing/Role/Result/Parent.pm
+++ lib/Wing/Role/Result/Parent.pm
@@ -5,32 +5,18 @@ use Ouch;
 use Moose::Role;
 with 'Wing::Role::Result::Field';
 
-sub register_parents {
-    my ($class, %fields) = @_;
-    while (my ($field, $definition) = each %fields) {
-        $class->register_parent($field, $definition);
-    }
-}
-
-sub register_parent {
+sub register_parent_field {
     my ($class, $field, $options) = @_;
     my $id = $options->{related_id} || $field.'_id';
     my %dbic = ( data_type => 'char', size => 36, is_nullable => 1 );
-    my @relationship = ($field, $options->{related_class}, $id);
     if ($options->{edit} ~~ [qw(required unique)]) {
         $dbic{is_nullable} = 0;
-    }
-    else {
-        push @relationship, { on_delete => 'set null' };
     }
     
     # create the field
     $options->{dbic} = \%dbic;
     $class->register_field($id, $options);
-    
-    # create relationship
-    $class->belongs_to(@relationship);
-    
+
     # validation
     $class->meta->add_before_method_modifier($id => sub {
         my ($self, $value) = @_;
@@ -51,6 +37,35 @@ sub register_parent {
         }
     });
 
+    # generate options
+    if ($options->{generate_options_by_name}) {
+        $class->meta->add_around_method_modifier(field_options => sub {
+            my ($orig, $self) = @_;
+            my $out = $orig->($self);
+            my @parent_ids;
+            my %parent_options;
+            my $parents = Wing->db->resultset($options->{related_class})->search(undef,{order_by => 'name'});
+            while (my $parent = $parents->next) {
+                push @parent_ids, $parent->id;
+                $parent_options{$parent->id} = $parent->name;
+            }
+            $out->{$id} = \@parent_ids;
+            $out->{'_'.$id} = \%parent_options;
+            return $out;
+        });
+    }
+}
+
+sub register_parent_relationship {
+    my ($class, $field, $options) = @_;
+    my $id = $options->{related_id} || $field.'_id';
+    my @relationship = ($field, $options->{related_class}, $id);
+    unless ($options->{edit} ~~ [qw(required unique)]) {
+        push @relationship, { on_delete => 'set null' };
+    }
+    # create relationship
+    $class->belongs_to(@relationship);
+    
     # add relationship to describe
     $class->meta->add_around_method_modifier(describe => sub {
         my ($orig, $self, %describe_options) = @_;
@@ -76,25 +91,18 @@ sub register_parent {
         }
         return $out;
     });
+}
 
-    # generate options
-    if ($options->{generate_options_by_name}) {
-        $class->meta->add_around_method_modifier(field_options => sub {
-            my ($orig, $self) = @_;
-            my $out = $orig->($self);
-            my @parent_ids;
-            my %parent_options;
-            my $parents = Wing->db->resultset($options->{related_class})->search(undef,{order_by => 'name'});
-            while (my $parent = $parents->next) {
-                push @parent_ids, $parent->id;
-                $parent_options{$parent->id} = $parent->name;
-            }
-            $out->{$id} = \@parent_ids;
-            $out->{'_'.$id} = \%parent_options;
-            return $out;
-        });
+sub register_parents {
+    my ($class, %fields) = @_;
+    while (my ($field, $options) = each %fields) {
+        $class->register_parent_field($field, $options);
+        $class->register_parent_relationship($field, $options);
     }
+}
 
+sub register_parent {
+    my ($class, $field, $options) = @_;
 }
 
 1;
