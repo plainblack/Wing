32e7fd02a1f5acdd85f583f7d072e43e6ff5ef45
JT Smith <jt@plainblack.com>
2012-07-08 22:06:33 -0500

    fix a couple bugs

---
+++ README.txt
@@ -0,0 +1,50 @@
+Wing is a modern web services platform written in Perl. It allows you to define objects and then automatically generates database schemas, restful web services, and even web form handlers to manage those objects. It's got built in users, sessions, single-sign-on and more.
+
+
+INSTALLATION
+
+1. Clone this repository into /data/Wing on your machine:
+
+  mkdir /data
+  cd /data
+  git clone git@github.com:plainblack/Wing.git
+
+2. Install the prereqs.
+
+  cd /data/Wing/bin/setup
+  ./01_download.sh
+  ./02_build.sh
+  ./03_install_perl_modules.sh
+  . /data/Wing/bin/dataapps.sh
+
+3. Create a project:
+
+  cd /data/Wing/bin
+  perl init_project.pl --project=MyProject
+
+4. Create a database on your MySQL server to host the project, and edit the Wing config to match:
+
+  mysql -uroot -p -e "create database my_project"
+  mysql -uroot -p -e "grant all privileges on my_project.* to some_user@localhost identified by 'some_pass'" 
+  mysql -uroot -p -e "flush privileges" 
+
+  vi /data/MyProject/etc/wing.conf  
+  # edit the "db" section
+
+5. Initialize the database:
+
+  cd /data/MyProject/bin/setup
+  export WING_CONFIG=/data/MyProject/etc/wing.conf
+  perl init_db.pl
+
+6. Start up the rest server:
+
+  cd /data/MyProject/bin
+  ./start_rest.sh
+
+7. Now you can connect to the rest server and see if it's alive:
+
+   curl http://localhost:5000/api/status
+
+
+
--- bin/init_project.pl
+++ bin/init_project.pl
@@ -29,30 +29,31 @@ $new_config->set("log4perl_config", "/data/".$project."/etc/log4perl.conf",);
 $new_config->write;
 
 # set up needed files
-my $tt = Template->new({INTERPOLATE => 1, EVAL_PERL => 1, ABSOLUTE => 1});
+my $tt = Template->new({ABSOLUTE => 1});
 my $vars = {project => $project};
 template($tt,'lib/DB.pm', $vars, 'lib/'.$project.'/DB.pm') || die $tt->error();
 template($tt,'lib/DB/Result/APIKey.pm', $vars, 'lib/'.$project.'/DB/Result/APIKey.pm');
-template($tt,'lib/DB/Result/APIKeyPermission.pm', $vars, 'lib/'.$project.'/DB/Result/APIKey.pm');
+template($tt,'lib/DB/Result/APIKeyPermission.pm', $vars, 'lib/'.$project.'/DB/Result/APIKeyPermission.pm');
 template($tt,'lib/DB/Result/User.pm', $vars, 'lib/'.$project.'/DB/Result/User.pm');
-template($tt,'etc/log4perl.conf', $vars, 'etc/log4perl.conf');
-template($tt,'etc/mime.types', $vars, 'etc/mime.types');
-template($tt,'bin/start_web.sh', $vars, 'bin/start_web.sh');
-template($tt,'bin/start_rest.sh', $vars, 'bin/start_rest.sh');
-template($tt,'bin/restart_starman.sh', $vars, 'bin/restart_starman.sh');
-template($tt,'bin/stop_starman.sh', $vars, 'bin/stop_starman.sh');
-template($tt,'bin/web.psgi', $vars, 'bin/web.psgi');
-template($tt,'bin/rest.psgi', $vars, 'bin/rest.psgi');
-template($tt,'bin/setup/install_perl_modules.sh', $vars, 'bin/setup/install_perl_modules.sh');
-template($tt,'bin/setup/init_db.pl', $vars, 'bin/setup/init_db.pl');
-template($tt,'bin/util/add_user.pl', $vars, 'bin/util/add_user.pl');
-template($tt,'bin/util/generate_init_sql.pl', $vars, 'bin/util/generate_init_sql.pl');
-template($tt,'bin/util/show_db_classes.pl', $vars, 'bin/util/show_db_classes.pl');
+template($tt,'etc/log4perl.conf', $vars);
+template($tt,'etc/mime.types', $vars);
+template($tt,'bin/start_web.sh', $vars);
+template($tt,'bin/start_rest.sh', $vars);
+template($tt,'bin/restart_starman.sh', $vars);
+template($tt,'bin/stop_starman.sh', $vars);
+template($tt,'bin/web.psgi', $vars);
+template($tt,'bin/rest.psgi', $vars);
+template($tt,'bin/setup/install_perl_modules.sh', $vars);
+template($tt,'bin/setup/init_db.pl', $vars);
+template($tt,'bin/util/add_user.pl', $vars);
+template($tt,'bin/util/generate_init_sql.pl', $vars);
+template($tt,'bin/util/show_db_classes.pl', $vars);
 
 # set privs
 system('cd /data/'.$project.'/bin;chmod 755 *');
 
 sub template {
   my ($tt, $from, $vars, $to) = @_;
+  $to ||= $from;
   $tt->process('/data/Wing/var/init/'.$from, $vars, '/data/'.$project.'/'.$to) || die $tt->error();
 }
