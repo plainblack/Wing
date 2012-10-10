4b50f0a4532e7c4a1400c2ad2d11c4d6cbc440a4
JT Smith <jt@plainblack.com>
2012-10-10 18:12:04 -0500

    documented the most important object

--- lib/Wing/DB/Result.pm
+++ lib/Wing/DB/Result.pm
@@ -5,9 +5,33 @@ use DateTime;
 use Ouch;
 use base 'DBIx::Class::Core';
 
+=head1 NAME
+
+Wing::DB::Result - The kernel of every object in Wing.
+
+=head1 SYNOPSIS
+
+ extends 'Wing::DB::Result';
+
+=cut
+
 __PACKAGE__->load_components('UUIDColumns', 'TimeStamp', 'InflateColumn::DateTime', 'InflateColumn::Serializer', 'Core');
 
+=head1 METHODS
+
+=head2 wing_apply_relationships()
+
+Power user feature.
+
+=cut
+
 sub wing_apply_relationships {}
+
+=head2 wing_apply_fields()
+
+Power user feature.
+
+=cut
 
 sub wing_apply_fields {
     my $class = shift;
@@ -20,6 +44,28 @@ sub wing_apply_fields {
     $class->uuid_columns('id');
 }
 
+=head2 wing_finalize_class(options)
+
+Call this this method once you have defined all fields and relationships in your object.
+
+=over
+
+=item options
+
+A hash.
+
+=over
+
+=item table_name
+
+The name of the table that should be generated for this object type.
+
+=back
+
+=back
+
+=cut
+
 sub wing_finalize_class {
     my ($class, %options) = @_;
     $class->table($options{table_name});
@@ -27,6 +73,12 @@ sub wing_finalize_class {
     $class->wing_apply_fields;
     $class->wing_apply_relationships;
 }
+
+=head2 new()
+
+Constructor. No parameters.
+
+=cut
 
 # override default DBIx::Class constructor to set defaults from schema
 sub new {
@@ -39,6 +91,12 @@ sub new {
     return $self;
 }
 
+=head2 wing_object_class()
+
+Returns the class name of the object, without the parent package path.
+
+=cut
+
 sub wing_object_class {
     my $self = shift;
     my $class = ref $self || $self;    
@@ -46,20 +104,78 @@ sub wing_object_class {
     return $class;
 }
 
+=head2 wing_object_name()
+
+Defaultly returns the same as C<wing_object_class> but should be overrriden to return a human friendly name for the object.
+
+=cut
+
 sub wing_object_name {
     my $self = shift;
     return $self->wing_object_class;
 }
+
+=head2 wing_object_type()
+
+Returns a lower case version of C<wing_object_class>. Used for addressing objects in URLs.
+
+=cut
 
 sub wing_object_type {
     my $self = shift;
     return lc($self->wing_object_class);
 }
 
+=head2 wing_object_api_uri()
+
+Returns the RESTful web service API URI for this object. If you do a GET request on this URI you'd fetch the object's public data.
+
+=cut
+
 sub wing_object_api_uri {
     my $self = shift;
     return '/api/'.$self->wing_object_type.'/'.$self->id;
 }
+
+=head2 describe(options)
+
+Serializes important data about the object and returns it as a hash reference.
+
+B<NOTE:> This method is context sensitive. It will return different data depending on the context information passed to it via the options hash.
+
+=over
+
+=item options
+
+A hash.
+
+=over
+
+=item include_options
+
+A boolean that indicates whether the describe output should include the result of the C<field_options> method.
+
+=item include_private
+
+A boolean that indicates whether the describe method should include sensitive data that only the owner of the object normally has access to.
+
+=item include_admin
+
+A boolean that indicates whether the describe method should include sensitive data that only admins normally have access to.
+
+=item include_relationships
+
+A boolean that inicates whether the describe method should include API links to this object's relationships.
+
+=item current_user
+
+A reference to a user object. If this is included then the object can determine whether to display private data on it's own.
+
+=back
+
+=back
+
+=cut
 
 sub describe {
     my ($self, %options) = @_;
@@ -79,14 +195,32 @@ sub describe {
     return $out;
 }
 
+=head2 field_options()
+
+Returns options for each field. Is wrapped by roles like L<Wing::Role::Result::Field> to expose enumerated options that some fields require.
+
+=cut
+
 sub field_options {
     return {};
 }
+
+=head2 touch()
+
+Updates the C<date_updated> field in the object to the current date.
+
+=cut
 
 sub touch {
     my $self = shift;
     $self->update({date_updated => DateTime->now});
 }
+
+=head2 sql_deploy_hook(sqlt_table)
+
+Is wrapped by roles like L<Wing::Role::Result::Field> to add special table indexes and other SQL deployment oddities.
+
+=cut
 
 sub sqlt_deploy_hook {
     my ($self, $sqlt_table) = @_;
@@ -94,17 +228,49 @@ sub sqlt_deploy_hook {
     $sqlt_table->add_index(name => 'idx_date_updated', fields => ['date_updated']);
 }
 
+=head2 postable_params()
+
+Is wrapped by roles like L<Wing::Role::Result::Field> to expose whether a Wing object's parameters should be postable.
+
+=cut
+
 sub postable_params {
     return [];
 }
+
+=head2 required_params()
+
+Is wrapped by roles like L<Wing::Role::Result::Field> to not allow an object instance to be created unless it includes these fields.
+
+=cut
 
 sub required_params {
     return [];
 }
 
+=head2 admin_postable_params()
+
+Is wrapped by roles like L<Wing::Role::Result::Field> to only allow this field to be postable by admin users.
+
+=cut
+
 sub admin_postable_params {
     return [];
 }
+
+=head2 can_use(user)
+
+Can this user use this object? 
+
+=over
+
+=item user
+
+A reference to a user object.
+
+=back
+
+=cut
 
 sub can_use {
     my ($self, $user) = @_;
@@ -112,12 +278,44 @@ sub can_use {
     ouch(450, 'Insufficient privileges.');
 }
 
+=head2 verify_creation_params(params, current_user)
+
+Used by web/rest interfaces to validate posted parameters for creation. Throws a 441 ouch if a required parameter is not present. 
+
+=over
+
+=item params
+
+A hash reference of parameters to verify.
+
+=item current_user
+
+A reference to the current user object.
+
+=cut
+
 sub verify_creation_params {
     my ($self, $params, $current_user) = @_;
     foreach my $param (@{$self->required_params}) {
         ouch(441, $param.' is required.', $param) unless $params->{$param} || $self->$param;
     }
 }
+
+=head2 verify_posted_params(params, current_user)
+
+Used by web/rest interfaces to validate posted parameters for update. Throws a 441 ouch if a required parameter is not present.
+
+=over
+
+=item params
+
+A hash reference of parameters to verify.
+
+=item current_user
+
+A reference to the current user object.
+
+=cut
 
 sub verify_posted_params {
     my ($self, $params, $current_user) = @_;
@@ -141,6 +339,12 @@ sub verify_posted_params {
         }
     }
 }
+
+=head2 duplicate()
+
+Duplicates this object with a new id. Can be extended to duplicate auxillary data.
+
+=cut
 
 sub duplicate {
     my ($self) = @_;
