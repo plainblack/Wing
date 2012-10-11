63ac259f0f37a997bcb3ec320ec8391104214a41
JT Smith <jt@plainblack.com>
2012-10-10 20:59:08 -0500

    added pod

--- lib/Wing/DB/ResultSet.pm
+++ lib/Wing/DB/ResultSet.pm
@@ -3,6 +3,21 @@ package Wing::DB::ResultSet;
 use Moose;
 extends 'DBIx::Class::ResultSet';
 
+=head1 NAME
+
+Wing::DB::ResultSet - A base class for result sets.
+
+=head1 SYNOPSIS
+
+with 'DBIx::Class::ResultSet';
+
+=head1 DESCRIPTION
+
+Right now this class doesn't do much. In the future we will add some base functionality.
+
+
+=cut
+
 sub BUILDARGS { $_[2] }
 
 no Moose;
