da499fb1dad1f757e9680831ecf289b93fcc86fe
Colin Kuskie <colink@perldreamer.com>
2012-11-03 12:53:05 -0700

    Move _common to the correct place and add an API key for admin in the deploy script.

--- var/init/dbicdh/_common/deploy/1/install_admin.pl
+++ var/init/dbicdh/_common/deploy/1/install_admin.pl
@@ -9,4 +9,14 @@ sub {
     $admin->admin(1);
     $admin->encrypt_and_set_password('123qwe');
     $admin->insert;
+
+    say "done adding admin...creating api key";
+
+    my $key = $db->resultset('APIKey')->new({});
+    $key->id('WEB000123456789012345678901234567890');
+    $key->name('[% project %]');
+    $key->user_id($admin->id);
+    $key->insert;
+
+    say "done creating api key...";
 }
