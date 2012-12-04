567de502fc905e9fd0d29d60e59d4451fefbb6d5
JT Smith <jt@plainblack.com>
2012-12-04 13:57:32 -0600

    update to the latest perl

--- bin/setup/02_build.sh
+++ bin/setup/02_build.sh
@@ -9,7 +9,7 @@ mv yuicompressor-2.4.7/build/yuicompressor-2.4.7.jar /data/apps/
 # on mac install X Code instead
 yum -y install ncurses-devel gcc make glibc-devel gcc-c++ zlib-devel openssl-devel java expat-devel glib2-devel
 
-cd perl-5.12.4
+cd perl-5.16.2
 ./Configure -Dprefix=/data/apps -des
 make
 make install
