994b3e969d318bf0c838327e88751079a295f1b6
Colin Kuskie <colink@perldreamer.com>
2013-01-06 19:41:09 -0800

    Yard all of YUI compressor out.

--- bin/setup/01_download.sh
+++ bin/setup/01_download.sh
@@ -1,4 +1,2 @@
 wget http://www.cpan.org/src/5.0/perl-5.16.2.tar.gz
 tar xfz perl-5.16.2.tar.gz
-wget http://yui.zenfs.com/releases/yuicompressor/yuicompressor-2.4.7.zip
-unzip yuicompressor-2.4.7.zip
