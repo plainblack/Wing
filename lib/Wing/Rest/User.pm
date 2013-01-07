89165a2fde45eb00553ce4dae4be429d17e31560
JT Smith <jt@plainblack.com>
2013-01-07 12:22:43 -0600

    better search

--- lib/Wing/Rest/User.pm
+++ lib/Wing/Rest/User.pm
@@ -8,7 +8,11 @@ use Wing::Rest;
 
 get '/api/user' => sub {
     ouch(450, 'You must be an administrator to get a list of all users.') unless get_user_by_session_id()->is_admin;
-    my $users = site_db()->resultset('User')->search(undef,{order_by => 'username'});
+    my $users = site_db()->resultset('User')->search({ -or => {
+        username    => { like => '%'.params->{query}.'%'}, 
+        email       => { like => '%'.params->{query}.'%'},
+        real_name   => { like => '%'.params->{query}.'%'},
+    }}, {order_by => 'username'});
     return format_list($users); 
 };
 
