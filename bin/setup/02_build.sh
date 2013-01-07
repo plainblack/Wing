8fb3eba9c36d9a1db59e956a3d1c2d05ce630ea5
JT Smith <jt@plainblack.com>
2013-01-07 11:24:38 -0600

    note for mac users

--- bin/setup/02_build.sh
+++ bin/setup/02_build.sh
@@ -2,10 +2,17 @@ mkdir -p /data/apps
 
 . /data/Wing/bin/dataapps.sh
 
-ln -s /etc/profile.d/dataapps.sh /data/Wing/bin/dataapps.sh
 
-# on mac install X Code instead
-yum -y install ncurses-devel gcc make glibc-devel gcc-c++ zlib-devel openssl-devel java expat-devel glib2-devel mysql-libs libxml2-devel mysql-common mysql-devel mysql
+# on mac install X Code and MySQL instead
+if [ uname=='Darwin' ]; then
+ echo "NOTICE:"
+ echo "You're on a mac. You need to install X Code and MySQL. If you have not already done so, please exit now and do so."
+ sleep 10
+else 
+ yum -y install ncurses-devel gcc make glibc-devel gcc-c++ zlib-devel openssl-devel expat-devel glib2-devel mysql-libs libxml2-devel mysql-common mysql-devel mysql
+fi
+
+ln -s /etc/profile.d/dataapps.sh /data/Wing/bin/dataapps.sh
 
 cd perl-5.16.2
 ./Configure -Dprefix=/data/apps -des
