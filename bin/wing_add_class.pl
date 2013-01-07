52b237e035bea956ec9a37ab826684c2191b5d8f
Colin Kuskie <colink@perldreamer.com>
2013-01-06 16:24:36 -0800

    Add a script to automatically add a class to a Wing project.

---
+++ bin/wing_add_class.pl
@@ -0,0 +1,48 @@
+#!/usr/bin/env perl
+
+BEGIN {
+ die "Must set WING_HOME environment variable." unless $ENV{WING_HOME};
+ die "Must set WING_APP environment variable." unless $ENV{WING_APP};
+ die "Must set WING_CONFIG environment variable." unless $ENV{WING_CONFIG};
+}
+
+use lib $ENV{WING_APP}.'/lib', $ENV{WING_HOME}.'/lib';
+
+use Wing;
+use Wing::Perl;
+use Template;
+use Getopt::Long;
+use File::Path qw(make_path);
+use Ouch;
+
+my $class_name;
+
+GetOptions("c|class=s" => \$class_name,);
+
+unless ($class_name) {
+    say "usage: ./wing_add_class --class=NewObject";
+    exit;
+}
+
+my $tt = Template->new({ABSOLUTE => 1});
+
+my $project = Wing->config->get('app_namespace');
+
+my $vars = {
+    project => $project,
+    class_name => $class_name,
+};
+
+eval {
+  $tt->process($ENV{WING_HOME}.'/var/add_class/DB/Result.tt', $vars, $ENV{WING_APP}.'/lib/'.$project.'/DB/Result/'.$class_name.'.pm') || die $tt->error();
+  $tt->process($ENV{WING_HOME}.'/var/add_class/Rest/Rest.tt', $vars, $ENV{WING_APP}.'/lib/'.$project.'/Rest/'.$class_name.'.pm') || die $tt->error();
+  $tt->process($ENV{WING_HOME}.'/var/add_class/Web/Web.tt', $vars, $ENV{WING_APP}.'/lib/'.$project.'/Web/'.$class_name.'.pm') || die $tt->error();
+};
+
+if ($@) {
+    say bleep;
+}
+else {
+    say $class_name, ' created';
+}
+
