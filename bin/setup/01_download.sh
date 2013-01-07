f70367952bb627766a9ca2b12c18e3d10788dffd
JT Smith <jt@plainblack.com>
2013-01-07 15:57:34 -0600

    automatically build nginx when on mac

--- bin/setup/01_download.sh
+++ bin/setup/01_download.sh
@@ -1,2 +1,6 @@
 wget http://www.cpan.org/src/5.0/perl-5.16.2.tar.gz
 tar xfz perl-5.16.2.tar.gz
+wget http://nginx.org/download/nginx-1.2.6.tar.gz
+tar xfz nginx-1.2.6.tar.gz
+wget http://superb-dca2.dl.sourceforge.net/project/pcre/pcre/8.32/pcre-8.32.tar.gz
+tar xfz pcre-8.32.tar.gz
--- bin/setup/02_build.sh
+++ bin/setup/02_build.sh
@@ -3,11 +3,16 @@ mkdir -p /data/apps
 . /data/Wing/bin/dataapps.sh
 
 
-# on mac install X Code and MySQL instead
+# on mac install X Code and MySQL instead and nginx
 if [ uname=='Darwin' ]; then
  echo "NOTICE:"
  echo "You're on a mac. You need to install X Code and MySQL. If you have not already done so, please exit now and do so."
  sleep 10
+ cd nginx-1.2.6
+ ./configure --prefix=/data/apps --with-pcre=../pcre-8.32
+ make
+ make install
+ 
 else 
  yum -y install ncurses-devel gcc make glibc-devel gcc-c++ zlib-devel openssl-devel expat-devel glib2-devel mysql-libs libxml2-devel mysql-common mysql-devel mysql
 fi
