c218c95b249b40bfe164d690cb8dd5add19b4d10
JT Smith <jt@plainblack.com>
2013-01-06 19:06:47 -0600

    added wing to the path

--- bin/dataapps.sh
+++ bin/dataapps.sh
@@ -1,4 +1,4 @@
-export PATH=/data/apps/bin:/data/apps/sbin:$PATH
+export PATH=/data/Wing/bin:/data/apps/bin:/data/apps/sbin:$PATH
 export LD_RUN_PATH=/data/apps/lib:$LD_RUN_PATH
 export LD_LIBRARY_PATH=/data/apps/lib:$LD_LIBRARY_PATH
 export ANY_MOOSE=Moose
