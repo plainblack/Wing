66db9bced3d5dcc6c3bd3565e90840b8582dd4f4
Colin Kuskie <colink@perldreamer.com>
2013-01-07 19:37:44 -0800

    Only switch users if you're root.

--- var/init/bin/start_rest.sh
+++ var/init/bin/start_rest.sh
@@ -2,5 +2,12 @@
 . /data/Wing/bin/dataapps.sh
 cd /data/[% project %]/bin
 export WING_CONFIG=/data/[% project %]/etc/wing.conf
+
+if [ $UID == 0 ] 
+  then
+	echo "switching root to nobody"
+	export RUNAS="--user nobody --group nobody"
+fi
+
 start_server --port 5000 -- starman --workers 2 --user nobody --group nobody --preload-app rest.psgi
 
