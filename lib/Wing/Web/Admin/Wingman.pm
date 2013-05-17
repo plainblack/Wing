7c883b8c62d1cc7e5489bf81e484aec6c4bbfb6d
JT Smith <jt@plainblack.com>
2013-05-17 08:02:14 -0700

    forgot to include user in wingman manger

--- lib/Wing/Web/Admin/Wingman.pm
+++ lib/Wing/Web/Admin/Wingman.pm
@@ -8,21 +8,28 @@ use Wing::Web;
 use Wingman;
 
 get '/admin/wingman' => sub {
+    my $user = get_user_by_session_id()->verify_is_admin();
     my $wingman = Wingman->new;
-    template 'admin/wingman', { stats => $wingman->stats_as_hashref };
+    template 'admin/wingman', {
+        stats => $wingman->stats_as_hashref,
+        current_user    => describe($user),
+    };
 };
 
 get '/admin/wingman/tubes/:tube' => sub {
+    my $user = get_user_by_session_id()->verify_is_admin();
     my $tube_name = params->{tube};
     my $wingman = Wingman->new;
     template 'admin/wingman_tube', {
         stats           => $wingman->stats_tube_as_hashref($tube_name),
         tube_name       => $tube_name,
         config          => Wing->config->get('wingman'),
+        current_user    => describe($user),
     };
 };
 
 get '/admin/wingman/jobs/:job_id' => sub {
+    my $user = get_user_by_session_id()->verify_is_admin();
     my $tube_name = params->{tube};
     my $job_id = params->{job_id};
     my $wingman = Wingman->new;
@@ -30,6 +37,7 @@ get '/admin/wingman/jobs/:job_id' => sub {
         job_id          => $job_id,
         config          => Wing->config->get('wingman'),
         stats           => $wingman->stats_job_as_hashref($job_id),
+        current_user    => describe($user),
     };
 };
 
