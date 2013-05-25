c32eaf69b064e6b769a9d8b9f64c163330198b1d
JT Smith <jt@plainblack.com>
2013-05-25 15:01:47 -0500

    added wing do

---
+++ lib/Wing/Command/Command/do.pm
@@ -0,0 +1,73 @@
+package Wing::Command::Command::do;
+
+use Wing;
+use Wing::Perl;
+use Wing::Command -command;
+use Ouch;
+use Wingman;
+use JSON;
+
+sub abstract { 'create Wingman jobs' }
+
+sub usage_desc { 'Create Wingman jobs from the command line.' }
+
+sub description { 'Examples:
+wing do HelloWorld
+
+wing do --delay=60 --priority=9999 HelloWorld 
+
+wing do --priority=500 EmailAllAdmins {\"template\":\"generic\", \"params\":{\"subject\":\"foo\", \"message\":\"bar\"}}
+
+'};
+
+sub opt_spec {
+    my $config = Wing->config->get('wingman/beanstalkd');
+    return (
+      [ 'delay=i', 'number of seconds to wait before adding the job to the waiting queue, default: '.($config->{delay} || 0 ), { default => $config->{delay} || 0 } ],
+      [ 'priority=i', 'the relative importance of this job, lower is more important, default: '.($config->{priority} || 2000), { default => $config->{priority} || 2000 } ],
+      [ 'ttr=i', 'number of seconds this job has to run, default: '.($config->{ttr} || 60), { default => $config->{ttr} || 60 } ],
+      [ 'tube=s', 'tube to put this job in, default: '.($config->{default_tube} || 'wingman'), { default => $config->{default_tube} || 'wingman' } ],
+    );
+}
+
+sub validate_args {
+    my ($self, $opt, $args) = @_;
+    # no args allowed but options!
+    $self->usage_error("Must at least specify a plugin phase.") unless @$args;
+}
+
+sub execute {
+    my ($self, $opt, $args) = @_;
+    my $phase = shift @{$args};
+    my $job_args = join ' ', @{$args}; # just in case they get broken up by the command line parser
+    if ($job_args) {
+        $job_args = decode_json($job_args);
+    }
+    else {
+        undef $job_args;
+    }
+    Wingman->new->put($phase, $job_args, $opt);
+    say "OK";
+}
+
+1;
+
+=head1 NAME
+
+wing user - Add and modify user accounts.
+
+=head1 SYNOPSIS
+
+ wing user --add --username=Joe --password=123qwe --admin
+
+ wing user --modify --username=Joe --noadmin
+ 
+=head1 DESCRIPTION
+
+This provides simple user management. For all complex function, you should use the web interface. 
+
+=head1 AUTHOR
+
+Copyright 2012-2013 Plain Black Corporation.
+
+=cut
