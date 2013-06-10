f91ee32bd81392e164938af6a3a27312c5b3f514
Colin Kuskie <colink@perldreamer.com>
2013-06-10 15:01:38 -0700

    Add restore site and list sites REST APIs.

--- lib/Wing/Rest/Site.pm
+++ lib/Wing/Rest/Site.pm
@@ -2,6 +2,7 @@ package Wing::Rest::Site;
 
 use Wing::Perl;
 use Dancer;
+use Wing::Dancer;
 use Ouch;
 use Wing::Rest;
 
@@ -15,6 +16,19 @@ del '/api/site/:id'  => sub {
     $object->can_edit(get_user_by_session_id());
     $object->trash;
     return { success => 1 };
+};
+
+put '/api/site/:id/restore' => sub {
+    my $object = fetch_object('Site');
+    $object->can_edit(get_user_by_session_id());
+    $object->restore;
+    return { success => 1 };
+};
+
+get '/api/sites' => sub {
+    my $current_user = get_user_by_session_id();
+    my $sites = Wing->db->resultset('Site')->search({user_id => $current_user->id, });
+    return format_list($sites, $current_user);
 };
 
 1;
