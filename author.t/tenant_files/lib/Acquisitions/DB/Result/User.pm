d630e44eee0c6effae69a143ebaa27c655511ca4
Colin Kuskie <colink@perldreamer.com>
2013-08-01 10:56:08 -0700

    Add tenant SSO role to the user class for testing.

--- author.t/tenant_files/lib/Acquisitions/DB/Result/User.pm
+++ author.t/tenant_files/lib/Acquisitions/DB/Result/User.pm
@@ -2,9 +2,11 @@ package Acquisitions::DB::Result::User;
 
 use Moose;
 use Wing::Perl;
-use Ouch;
 extends 'Wing::DB::Result';
 with 'Wing::Role::Result::User';
+with 'Wing::Role::Result::UserTenantSSO';
+
+use constant syncable_fields => qw/username email real_name use_as_display_name password password_salt password_type/;
 
 __PACKAGE__->wing_finalize_class( table_name => 'users');
 
