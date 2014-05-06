1e495858f0960da5e0415801d1006d3ba4efdc44
Colin Kuskie <colink@perldreamer.com>
2014-05-06 16:38:01 -0700

    Add Wing::Template to init class Web.pm file.

--- var/init/lib/Web.pm
+++ var/init/lib/Web.pm
@@ -2,6 +2,7 @@ package [% project %]::Web;
 
 use Dancer;
 # your modules here
+use Wing::Template; ## Should be the LAST hook added for processing templates.
 use Wing::Web::Account;
 use Wing::Web::Admin::User;
 use Wing::Web::Admin::Wingman;
