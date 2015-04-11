1610b88d24f23f0bf24b52906a5bc755d0de3eb4
JT Smith <jt@plainblack.com>
2015-04-11 18:00:51 -0500

    added unshift option to create and timeago filter

--- public/wing.angular.js
+++ public/wing.angular.js
@@ -20,6 +20,80 @@ angular.module('wing',[])
 .filter('datetime', ['$filter', wing.angular_datetime_filter])
 
 /*
+ * Converts a wing datetime field into a human readable string
+ *
+ * Borrowed from: https://github.com/uttesh/ngtimeago
+ */
+.filter('timeago', function() {
+    return function(input, p_allowFuture) {
+    
+        var substitute = function (stringOrFunction, number, strings) {
+                var string = angular.isFunction(stringOrFunction) ? stringOrFunction(number, dateDifference) : stringOrFunction;
+                var value = (strings.numbers && strings.numbers[number]) || number;
+                return string.replace(/%d/i, value);
+            },
+            nowTime = (new Date()).getTime(),
+            datetime = input.replace(/\s/,'T') + 'Z';
+            date = (new Date(datetime)).getTime();
+            //refreshMillis= 6e4, //A minute
+            allowFuture = p_allowFuture || false,
+            strings= {
+                prefixAgo: '',
+                prefixFromNow: '',
+                suffixAgo: "ago",
+                suffixFromNow: "from now",
+                seconds: "less than a minute",
+                minute: "about a minute",
+                minutes: "%d minutes",
+                hour: "about an hour",
+                hours: "about %d hours",
+                day: "a day",
+                days: "%d days",
+                month: "about a month",
+                months: "%d months",
+                year: "about a year",
+                years: "%d years"
+            },
+            dateDifference = nowTime - date,
+            words = 0,
+            seconds = Math.abs(dateDifference) / 1000,
+            minutes = seconds / 60,
+            hours = minutes / 60,
+            days = hours / 24,
+            years = days / 365,
+            separator = strings.wordSeparator === undefined ?  " " : strings.wordSeparator,
+        
+           
+            prefix = strings.prefixAgo,
+            suffix = strings.suffixAgo;
+            
+        if (allowFuture) {
+            if (dateDifference < 0) {
+                prefix = strings.prefixFromNow;
+                suffix = strings.suffixFromNow;
+            }
+        }
+
+        words = seconds < 45 && substitute(strings.seconds, Math.round(seconds), strings) ||
+        seconds < 90 && substitute(strings.minute, 1, strings) ||
+        minutes < 45 && substitute(strings.minutes, Math.round(minutes), strings) ||
+        minutes < 90 && substitute(strings.hour, 1, strings) ||
+        hours < 24 && substitute(strings.hours, Math.round(hours), strings) ||
+        hours < 42 && substitute(strings.day, 1, strings) ||
+        days < 30 && substitute(strings.days, Math.round(days), strings) ||
+        days < 45 && substitute(strings.month, 1, strings) ||
+        days < 365 && substitute(strings.months, Math.round(days / 30), strings) ||
+        years < 1.5 && substitute(strings.year, 1, strings) ||
+        substitute(strings.years, Math.round(years), strings);
+        prefix.replace(/ /g, '')
+        words.replace(/ /g, '')
+        suffix.replace(/ /g, '')
+        return (prefix+' '+words+' '+suffix+' '+separator);
+        
+    };
+})
+
+/*
  * Confirmations manager
  */
 
@@ -141,7 +215,12 @@ angular.module('wing',[])
             var self = this;
             var new_object = self._create_object_manager(properties);
             var add_it = function() {
-                self.objects.push(new_object);
+                if (typeof options !== 'undefined' && typeof options.unshift !== 'undefined' && options.unshift == true) {
+                    self.objects.unshift(new_object);
+                }
+                else {
+                    self.objects.push(new_object);
+                }
             };
             if (typeof options !== 'undefined') {
                 if (typeof options.on_success !== 'undefined') {
@@ -378,4 +457,5 @@ angular.module('wing',[])
         }
     }
 }])
+
 ;
