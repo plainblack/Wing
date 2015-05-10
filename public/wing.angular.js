4dec0c9aaa6f516ea6afe8e88def92dd082240b1
JT Smith <jt@plainblack.com>
2015-05-10 17:59:51 -0500

    fix ie caching issue

--- public/wing.angular.js
+++ public/wing.angular.js
@@ -11,6 +11,20 @@ angular.module('wing',[])
 .config( function($httpProvider) {
     $httpProvider.defaults.withCredentials = true;
     $httpProvider.interceptors.push(wing.angular_http_interceptor);
+
+    //initialize get if not there
+    if (!$httpProvider.defaults.headers.get) {
+        $httpProvider.defaults.headers.get = {};    
+    }    
+
+    // Answer edited to include suggestions from comments
+    // because previous version of code introduced browser-related errors
+
+    //disable IE ajax request caching
+    $httpProvider.defaults.headers.get['If-Modified-Since'] = 'Mon, 26 Jul 1997 05:00:00 GMT';
+    // extra
+    $httpProvider.defaults.headers.get['Cache-Control'] = 'no-cache';
+    $httpProvider.defaults.headers.get['Pragma'] = 'no-cache';
 })
 
 /*
