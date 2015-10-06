3a5dfc0c8f2288603a391b2e844e07cdba0bab93
JT Smith <jt@plainblack.com>
2015-10-06 16:46:14 -0500

    don't need that afterall, needed sudo install_name_tool -change libmysqlclient.18.dylib /usr/local/mysql/lib/libmysqlclient.18.dylib /data/apps/lib/perl5/site_perl/5.16.2/darwin-2level/auto/DBD/mysql/mysql.bundle

--- bin/dataapps.sh
+++ bin/dataapps.sh
@@ -1,6 +1,5 @@
 export PATH=/data/Wing/bin:/data/apps/bin:/data/apps/sbin:$PATH
 export LD_RUN_PATH=/data/apps/lib:$LD_RUN_PATH
 export LD_LIBRARY_PATH=/data/apps/lib:$LD_LIBRARY_PATH
-export DYLD_LIBRARY_PATH=/usr/local/mysql/lib
 export ANY_MOOSE=Moose
 export WING_HOME=/data/Wing
