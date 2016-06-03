355735befac30b88942d17ac245345630e8735ef
JT Smith <jt@plainblack.com>
2016-06-03 12:58:48 -0500

    Added wait_on_wingman() and cleanup_wait_on_wingman() to Wing::TestHelper.

--- lib/Wing/TestHelper.pm
+++ lib/Wing/TestHelper.pm
@@ -12,7 +12,7 @@ use HTTP::Request::Common;
 use Wing;
 use Wing::Perl;
 use Wing::Rest::Session;
-
+use Wingman;
 use Moose;
 
 has debug_enabled => (
@@ -25,16 +25,20 @@ has user => (
     lazy    => 1,
     default => sub {
         my $self = shift;
-        my $andy = Wing->db->resultset('User')->new({
-            username    => 'andy',
-            real_name   => 'Andy Dufresne',
-            email       => 'andy@shawshank.jail',
-            admin       => 1,
-            developer   => 1,
-            use_as_display_name => 'real_name', 
-        });
-        $andy->encrypt_and_set_password('Saywatanayo');
-        $andy->insert;    
+        my $users = Wing->db->resultset('User');
+        my $andy = $users->find({username => 'andy'});
+        unless (defined $andy) {
+            $andy = $users->new({
+                username    => 'andy',
+                real_name   => 'Andy Dufresne',
+                email       => 'andy@shawshank.jail',
+                admin       => 1,
+                developer   => 1,
+                use_as_display_name => 'real_name', 
+            });
+            $andy->encrypt_and_set_password('Saywatanayo');
+            $andy->insert;    
+        }
         $self->add_to_cleanup($andy);
         return $andy;
     }
@@ -45,8 +49,12 @@ has apikey => (
     lazy    => 1,
     default => sub {
         my $self = shift;
-        my $key = Wing->db->resultset('APIKey')->new({user_id => $self->user->id, name => 'Key for Andy', });
-        $key->insert;
+        my $keys = Wing->db->resultset('APIKey');
+        my $key = $keys->find({name => 'Key for Andy'});
+        unless (defined $key) {
+            $key = $keys->new({user_id => $self->user->id, name => 'Key for Andy', });
+            $key->insert;
+        }
         $self->add_to_cleanup($key);
         return $key;
     }
@@ -65,6 +73,29 @@ has things_to_cleanup => (
 sub add_to_cleanup {
     my $self = shift;
     push @{$self->things_to_cleanup}, [@_];
+}
+
+sub wait_on_wingman {
+    my $self = shift;
+        my $timeout = 0;
+        print "Waiting for Wingman processes to complete";
+        while ($timeout < 60) {
+            my $stats = Wingman->new->stats_as_hashref;
+            if ($stats->{current_jobs_ready} + $stats->{current_jobs_reserved} + $stats->{current_jobs_urgent} + $stats->{current_jobs_delayed} == 0) {
+                last;
+            }
+            print ".";
+            sleep 1;
+            $timeout++;
+        }
+        print "\n";
+}
+
+sub cleanup_wait_on_wingman {
+    my $self = shift;
+    $self->add_to_cleanup(sub {
+        $self->wait_on_wingman;
+    });
 }
 
 sub rest {
@@ -111,7 +142,10 @@ sub rest_via_plack_test { # for file uploads
 sub cleanup {
     my $self = shift;
     foreach my $thing (reverse @{$self->things_to_cleanup}) {
-        if (ref $thing->[0]) {
+        if (ref $thing->[0] eq 'CODE') {
+            $thing->[0]->();
+        }
+        elsif (ref $thing->[0]) {
             eval { 
                 if ($self->debug_enabled) {
                     say "Cleaning up ".ref($thing->[0])." with id ".$thing->[0]->id;
