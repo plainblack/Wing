6a804037116869f1032c3dbae1adf4affe993b43
JT Smith <jt@plainblack.com>
2016-09-30 21:53:56 -0500

    Revert "User REST should generate all relationships by default"
    This reverts commit c687e5f5254fb2a5087e304a881940e02258e164.

--- lib/Wing/Rest/User.pm
+++ lib/Wing/Rest/User.pm
@@ -27,7 +27,4 @@ generate_update('User', permissions => ['edit_my_account']);
 generate_delete('User', permissions => ['edit_my_account']);
 generate_create('User', extra_processing => $extra);
 
-generate_all_relationships('User');
-
-
 1;
