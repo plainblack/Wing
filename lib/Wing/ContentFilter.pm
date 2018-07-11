7582508072a0fc4d382cf9465d904963212e0c09
colin <colink@perldreamer.com>
2018-07-11 10:02:42 -0700

    Add a very basic Javascript neutralizer to Wing::ContentFilter.

--- lib/Wing/ContentFilter.pm
+++ lib/Wing/ContentFilter.pm
@@ -20,6 +20,11 @@ sub neutralize_html {
     ${$content} =~ s/\>/&gt;/g;
 }
 
+sub neutralize_javascript {
+    my ($content) = @_;
+    ${$content} =~ s{<\s*/?\s*script}{}g;
+}
+
 sub format_markdown {
     my ($content) = @_;
     my $m = Wing::Markdown->new(
