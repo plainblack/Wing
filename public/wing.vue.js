a7d0ab42e05913823d3b42dcdea303f52a35e8ea
JT Smith <jt@plainblack.com>
2018-07-17 13:21:10 -0500

    Added bytes filter to wing.vue.js.

--- public/wing.vue.js
+++ public/wing.vue.js
@@ -104,6 +104,19 @@ Vue.filter('round', function(number, precision){
     return shift(Math.round(shift(number, precision, false)), precision, true);
 });
 
+
+/*
+ * format a file size using common bytes multiples
+ */
+
+Vue.filter('bytes', function(bytes){
+    if (isNaN(parseFloat(bytes)) || !isFinite(bytes)) return '-';
+    if (typeof precision === 'undefined') precision = 1;
+    var units = ['bytes', 'kB', 'MB', 'GB', 'TB', 'PB'],
+        number = Math.floor(Math.log(bytes) / Math.log(1024));
+    return (bytes / Math.pow(1024, Math.floor(number))).toFixed(precision) +  ' ' + units[number];
+});
+
 /*
  * Automatically save an input field.
  */
