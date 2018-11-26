1a72758a784308e680310923eb7135db5ae3376e
JT Smith <jt@plainblack.com>
2018-11-25 22:51:14 -0600

    Added Wing::Instrument to make it easier to profile Wing.

---
+++ lib/Wing/Instrument.pm
@@ -0,0 +1,49 @@
+package Wing::Instrument;
+
+use Wing::Perl;
+use Wing;
+use Moo;
+use Time::HiRes;
+use JSON;
+
+has id => (
+    is => 'rw',
+    default => sub { time() },
+);
+
+has sensitivity => (
+    is => 'rw',
+    default => 0.5,
+);
+
+has recordings => (
+    is => 'rw',
+    default => sub { [] },
+);
+
+has start => (
+    is => 'ro',
+    default => sub { [Time::HiRes::gettimeofday] },
+);
+
+sub interval {
+    my $self = shift;
+    Time::HiRes::tv_interval($self->start)
+}
+
+sub record {
+   my ($self, $name) = @_;
+   push @{$self->recordings}, { $name => $self->interval };
+}
+
+sub as_json {
+    my $self = shift;
+    return encode_json($self->recordings);
+}
+
+sub log {
+    my ($self, $label) = @_;
+    Wing->log->warn(sprintf('INSTRUMENT: %s (%s) = %s', $label, $self->id, $self->as_json)) if $self->interval > $self->sensitivity;
+}
+
+1;
