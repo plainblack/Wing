6d8bce0668493f27dfa6b0c6bc4314b5b7205c49
colin <colink@perldreamer.com>
2019-04-16 15:03:27 -0700

    Use Text::Demoroniser to clean messages being sent to Firebase

--- lib/Wing/Firebase.pm
+++ lib/Wing/Firebase.pm
@@ -5,6 +5,7 @@ use Moo;
 
 extends 'Firebase';
 use Time::HiRes;
+use Text::Demoroniser qw/demoroniser_utf8/;
 
 around BUILDARGS => sub {
     my $orig = shift;
@@ -23,7 +24,11 @@ sub object_status {
     my $self      = shift;
     my $object    = shift;
     my $directory = join '/', $self->directory, $object->id, int(Time::HiRes::time()*1000);
-    return $self->put($directory, @_);
+    my $payload   = shift;
+    if (ref($payload) eq 'HASH' && exists $payload->{message}) {
+        $payload->{message} = demoroniser_utf8($payload->{message});
+    }
+    return $self->put($directory, $payload, @_);
 }
 
 1;
