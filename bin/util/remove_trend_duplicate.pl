b29a6a08ba0009edaf5a5ab4d1a97f73790feb93
colin <colink@perldreamer.com>
2019-07-11 12:55:51 -0700

    Allow it to work on multiple names per date spec

--- bin/util/remove_trend_duplicate.pl
+++ bin/util/remove_trend_duplicate.pl
@@ -5,6 +5,7 @@ use lib $ENV{WING_APP}.'/lib', '/data/Wing/lib';
 use Wing::Perl;
 use Wing;
 use Getopt::Long;
+use Wing::TrendsLogger qw//;
 
 GetOptions(
     'h|hour'  => \my $hour,
@@ -12,20 +13,23 @@ GetOptions(
     'm|month' => \my $month,
     'y|year'  => \my $year,
     'fix'     => \my $fix,
+    'names'   => \my $names,
 );
 
-my ($date, $name);
+my ($date, $single_name);
 
 if (scalar @ARGV == 3) {
     $date = shift(@ARGV) . ' '. shift(@ARGV);
-    $name = shift(@ARGV)
+    $single_name = shift(@ARGV)
 }
 else {
     $date = shift @ARGV;
-    $name = shift @ARGV
+    $single_name = shift @ARGV
 }
 
-die "Must supply date and name\n" unless $date && $name;
+die "Must supply date and name\n" unless $date && ($single_name || $names);
+
+my @names = $names ? Wing::TrendsLogger::trend_names() : ($single_name);
 
 my ($table_name, $date_name);
 
@@ -48,21 +52,25 @@ elsif ($year) {
 
 my $rs = Wing->db->resultset($table_name);
 
-my $entries = $rs->search({$date_name => $date, name => $name, }, {order_by => { -desc => [qw/date_created date_updated/], } });
-my $count = $entries->count;
+my $date_entries = $rs->search({$date_name => $date, },);
 
-say "$hour $name has $count entries in table $table_name";
+foreach my $name (@names) {
+    my $entries = $date_entries->search({name => $name, }, {order_by => { -desc => [qw/date_created date_updated/], } });
+    my $count = $entries->count;
 
-my $skip_first = 1;
-while (my $entry = $entries->next) {
-    say join ' ', $entry->id, $entry->date_created, $entry->date_updated, $entry->value;
-    if ($fix) {
-        if ($skip_first) {
-            $skip_first = 0;
-        }
-        else {
-            say "Deleting older duplicate";
-            $entry->delete;
+    say "$hour $name has $count entries in table $table_name";
+
+    my $skip_first = 1;
+    while (my $entry = $entries->next) {
+        say join ' ', $entry->id, $entry->date_created, $entry->date_updated, $entry->value;
+        if ($fix) {
+            if ($skip_first) {
+                $skip_first = 0;
+            }
+            else {
+                say "Deleting older duplicate";
+                $entry->delete;
+            }
         }
     }
 }
