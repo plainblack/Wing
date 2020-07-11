a7ab2d264eea52b43c48c1c217b6877c1ff6a209
JT Smith <jt@plainblack.com>
2020-07-10 19:48:26 -0500

    initial version to see what colin says

---
+++ lib/Wing/Util.pm
@@ -0,0 +1,42 @@
+package Wing::Util;
+use List::MoreUtils qw(any);
+
+require Exporter;
+@ISA = qw(Exporter);
+@EXPORT_OK = qw(
+    randint
+    format_date
+    random_element
+    commify
+    consolidate_items
+    kmbtq
+    real_ip_address
+    );
+
+
+# Return a random integer between $low and $high inclusive
+sub randint {
+    my ($low, $high) = @_;
+    $low = 0 unless defined $low;
+    $high = 1 unless defined $high;
+    ($low, $high) = ($high,$low) if $low > $high;
+    return int($low + int( rand( $high - $low + 1 ) ));
+}
+
+sub random_element {
+    my ($list) = @_;
+    return $list->[randint(0, scalar(@{$list} -1 ))];
+}
+
+sub commify {
+    my $text = reverse $_[0];
+    $text =~ s/(\d\d\d)(?=\d)(?!\d*\.)/$1,/g;
+    return scalar reverse $text;
+}
+
+sub is_in {
+
+}
+
+1;
+
