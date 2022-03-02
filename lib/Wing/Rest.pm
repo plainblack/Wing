197117a8a5bcbc9164b2a01ecd3cdc647ab533c8
JT Smith <jt@plainblack.com>
2022-03-02 13:38:28 -0600

    debug

--- lib/Wing/Rest.pm
+++ lib/Wing/Rest.pm
@@ -85,19 +85,31 @@ register generate_create => sub {
     my ($wing_object_type, %options) = @_;
     my $object_url = lc($wing_object_type);
     post '/api/'.$object_url => sub {
+Wing->log->debug($object_url. ' starting');
         my $db_class_name = %options && exists $options{db_class_name} ? $options{db_class_name} : $wing_object_type; # creates alias for migrating from old APIs to new
+Wing->log->debug($object_url. ' a');
         my $object = site_db()->resultset($db_class_name)->new({});
+Wing->log->debug($object_url. ' b');
         my $current_user = eval { get_user_by_session_id(permissions => $options{permissions}); };
+Wing->log->debug($object_url. ' c');
         my $params = expanded_params($current_user);
+Wing->log->debug($object_url. ' d');
         if ($db_class_name ne $wing_object_type) { # provides an identity for migrating old APIs in case that is needed
+Wing->log->debug($object_url. ' e');
             $params->{identity} = $wing_object_type;  
         }
+Wing->log->debug($object_url. ' f');
         $object->verify_creation_params($params, $current_user);
+Wing->log->debug($object_url. ' g');
         $object->verify_posted_params($params, $current_user, get_tracer());
+Wing->log->debug($object_url. ' h');
         if (defined $options{extra_processing}) {
+Wing->log->debug($object_url. ' i');
             $options{extra_processing}->($object, $current_user);
         }
+Wing->log->debug($object_url. ' j');
         $object->insert;
+Wing->log->debug($object_url. ' ending');
         return describe($object, current_user => $current_user);
     };
 };
